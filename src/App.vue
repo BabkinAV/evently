26be6720c1c50a674c09a71b8bf4bae9a2e5e087
Andrey Babkin <babkin.andrey@gmail.com>
2023-08-30 09:58:28 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import CardItem from './components/CardItem.vue';
+</script>
+
+<template>
+	<CardItem />
+	<!-- TODO: Add headless ui to the project  https://headlessui.com/-->
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+		<p>This is the welcome view</p>
+  </main>
+</template>
