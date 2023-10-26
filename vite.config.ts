7edcbdff0e5c5f62dbfe3dc40ac93ff3dda82745
Andrey Babkin <babkin.andrey@gmail.com>
2023-10-26 15:57:21 +0300

    added dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+		host: true,
     port: 3000
   },
+	host: true,
   plugins: [vue()],
   resolve: {
     alias: {
